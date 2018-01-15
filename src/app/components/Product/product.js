e59ff321ad110dbb60686dd14f44f6f3cc7d5c3e
klhz1 <vahterovs@gmail.com>
2018-01-15 22:15:16 +0100

    styling

--- src/app/components/Product/product.js
+++ src/app/components/Product/product.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import './product.scss';
 
 const Product = ({product}) =>
-    <li>
-        <img />
-        <h2>{ product.get('name') }</h2>
-        <p>{ product.get('shortDescription') }</p>
-        <h4>{ product.get('retailerCount') } butiker</h4>
-        <h2>fr. { product.get('localMinPrice').get('value')}</h2>
-        <a href={ product.get('comparePricesLink') }>Jämför</a>
+    <li className = "product">
+        <img className = "product-img" />
+        <a className = "product-info-block">
+            <h2 className = "product-title">{ product.get('name') }</h2>
+            <p className = "product-text">{ product.get('shortDescription') }</p>
+            <div className = "product-price-block">
+                <h4 className = "product-retailer-count">{ product.get('retailerCount') } butiker</h4>
+                <h2 className = "product-price-from">fr. { product.get('localMinPrice').get('value')}</h2>
+            </div>
+        </a>
+        <div  className = "product-link-compare" href = { product.get('comparePricesLink') }>Jämför pris</div>
+        
     </li>;
 
 export default Product;
