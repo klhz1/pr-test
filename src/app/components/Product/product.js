24f1925503f9f08b738e7aefd3bed14dd203124d
Anatolijs Vahterovs <Anatolijs.Vahterovs@pricerunner.com>
2018-01-16 17:55:27 +0100

    styling 2

--- src/app/components/Product/product.js
+++ src/app/components/Product/product.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './product.scss';
+import { getProductImageLink } from '../../../app/utils/product.js';
 
 const Product = ({product}) =>
     <li className = "product">
-        <img className = "product-img" />
+        <img className = "product-img" src = {getProductImageLink(product)} />
         <a className = "product-info-block">
-            <h2 className = "product-title">{ product.get('name') }</h2>
+            <h3 className = "product-title">{ product.get('name') }</h3>
             <p className = "product-text">{ product.get('shortDescription') }</p>
             <div className = "product-price-block">
-                <h4 className = "product-retailer-count">{ product.get('retailerCount') } butiker</h4>
-                <h2 className = "product-price-from">fr. { product.get('localMinPrice').get('value')}</h2>
+                <span className = "product-retailer-count">{ product.get('retailerCount') } butiker</span>
+                <span className = "product-price-from">fr. { product.get('localMinPrice').get('value')}</span>
             </div>
         </a>
         <div  className = "product-link-compare" href = { product.get('comparePricesLink') }>Jämför pris</div>
-        
+
     </li>;
 
 export default Product;
--- src/app/components/ProductList/productList.js
+++ src/app/components/ProductList/productList.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './productList.scss';
 import Product from '../Product/product.js';
 
 const ProductList = ({list}) =>
   <div>
-        <ul>
+        <ul className="product-list-cont">
             { list.map(product =>
                 <Product  key = { product.get('id') } product = { product } />
             )}
