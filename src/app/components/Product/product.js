2dff193d6f9a3a7ba9c1a31fdebc750f4b5b9d8f
klhz1 <vahterovs@gmail.com>
2018-01-16 22:49:48 +0100

    styling 3

--- src/app/components/Product/product.js
+++ src/app/components/Product/product.js
@@ -4,17 +4,16 @@ import { getProductImageLink } from '../../../app/utils/product.js';
 
 const Product = ({product}) =>
     <li className = "product">
-        <img className = "product-img" src = {getProductImageLink(product)} />
-        <a className = "product-info-block">
+        <img className = "product-img" src = { getProductImageLink(product) } />
+        <a className = "product-info-block" href = { product.get('comparePricesLink') }>
             <h3 className = "product-title">{ product.get('name') }</h3>
             <p className = "product-text">{ product.get('shortDescription') }</p>
             <div className = "product-price-block">
                 <span className = "product-retailer-count">{ product.get('retailerCount') } butiker</span>
-                <span className = "product-price-from">fr. { product.get('localMinPrice').get('value')}</span>
+                <span className = "product-price-from">fr. { product.get('localMinPrice').get('value').replace(/\.\d+$/,'') } kr</span>
             </div>
         </a>
-        <div  className = "product-link-compare" href = { product.get('comparePricesLink') }>Jämför pris</div>
-
+        <a  className = "product-link-compare" href = { product.get('comparePricesLink') }>Jämför pris</a>
     </li>;
 
 export default Product;
